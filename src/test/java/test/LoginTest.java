e3e93a434e3489c26adc2dd7a3f6700b9b240b0a
Anh <theringofhell12@gmail.com>
2020-11-03 21:19:02 +0700

    Initial Commit

---
+++ src/test/java/test/LoginTest.java
@@ -0,0 +1,30 @@
+package test;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+
+public class LoginTest {
+	public static WebDriver driver;
+	
+	@BeforeSuite
+	public void setUp() {
+		
+		driver = new FirefoxDriver();
+	}
+	
+	@Test
+	public void doLogin() {
+		driver.get("http:gmail.com");
+		driver.findElement(By.id("Email")).sendKeys("w2automation");
+	}
+	
+	@AfterSuite
+	public void tearDown() {
+		driver.quit();
+	}
+}
